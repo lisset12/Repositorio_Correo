4e25e08cbe144ff5d5405a0b8c7805356a0d2053
lisset12 <lizmarian12769@gmail.com>
2017-05-26 13:36:14 -0500

    Correo

---
+++ EnviarCorreo.Nativo/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button button;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            button = FindViewById<Button>(Resource.Id.btn_Reportar);
+            button.Click += btnReportar_Click;
+
+        }
+
+        private async void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper serviceHelper = new ServiceHelper();
+
+            await serviceHelper.InsertarEntidad("lizmar-127@hotmail.com", "UPMP");
+
+            button.Text = "Reporte enviado";
+        }
+    }
+}
+
---
+++ EnviarCorreo.Nativo/EnviarCorreo.Nativo/ServiceHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EnviarCorreo.Nativo
+{
+    internal class ServiceHelper
+    {
+        internal Task InsertarEntidad(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
